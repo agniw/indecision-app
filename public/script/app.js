52b8d6a6f96e116f230f88e753cebdf3d98e2f64
Agnieszka Wojsznis <agnieszka.wojsznis@gmail.com>
2018-10-02 23:18:15 +0200

    Setting up Babel

---
+++ public/script/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var template = React.createElement(
+  'p',
+  null,
+  ' React '
+);
+
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
---
+++ src/app.js
@@ -0,0 +1,5 @@
+var template = <p> React </p>;
+
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
