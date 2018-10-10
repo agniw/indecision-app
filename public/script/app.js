3d056737ae2cf086283504358aa9b6661f1ac8d7
Agnieszka Wojsznis <agnieszka.wojsznis@gmail.com>
2018-10-10 21:39:11 +0200

    little counter

--- public/script/app.js
+++ public/script/app.js
@@ -1,11 +1,51 @@
 'use strict';
 
-var template = React.createElement(
-  'p',
-  null,
-  ' React '
-);
-
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+var count = 0;
+
+var addOne = function addOne() {
+    count++;
+    renderCounterApp();
+};
+var minusOne = function minusOne() {
+    count--;
+    renderCounterApp();
+};
+var reset = function reset() {
+    count = 0;
+    renderCounterApp();
+};
+
+var renderCounterApp = function renderCounterApp() {
+    var template = React.createElement(
+        'div',
+        null,
+        React.createElement(
+            'h1',
+            null,
+            ' Count: ',
+            count,
+            ' '
+        ),
+        React.createElement(
+            'button',
+            { id: 'my-id', className: 'button', onClick: addOne },
+            ' + 1 '
+        ),
+        React.createElement(
+            'button',
+            { id: 'my-id', className: 'button', onClick: minusOne },
+            ' - 1 '
+        ),
+        React.createElement(
+            'button',
+            { id: 'my-id', className: 'button', onClick: reset },
+            ' reset '
+        )
+    );
+
+    ReactDOM.render(template, appRoot);
+};
+
+renderCounterApp();
--- src/app.js
+++ src/app.js
@@ -1,5 +1,31 @@
-var template = <p> React </p>;
+const appRoot = document.getElementById('app');
 
-var appRoot = document.getElementById('app');
+let count = 0;
 
-ReactDOM.render(template, appRoot);
+const addOne = () => {
+    count++;
+    renderCounterApp();
+};
+const minusOne = () => {
+    count--;
+    renderCounterApp();
+};
+const reset = () => {
+    count = 0;
+    renderCounterApp();
+};
+
+const renderCounterApp = () => {
+    const template = (
+        <div>
+            <h1> Count: {count} </h1>
+            <button id='my-id' className='button' onClick={addOne}> + 1 </button>
+            <button id='my-id' className='button' onClick={minusOne}> - 1 </button>
+            <button id='my-id' className='button' onClick={reset}> reset </button>
+        </div>
+    );
+
+    ReactDOM.render(template, appRoot);
+};
+
+renderCounterApp();
