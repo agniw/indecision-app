51d5e1bc375b0b4c63e9b61c3243b8eb76aad314
Agnieszka Wojsznis <agnieszka.wojsznis@protonmail.com>
2019-01-11 12:39:53 +0100

    One component per file

---
+++ src/components/Action.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Action = props => {
+    return (
+      <button onClick={props.handlePick} disabled={!props.hasOptions}>
+        {" "}
+        What should I do?{" "}
+      </button>
+    );
+  };
+
+  export default Action;
--- src/test.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-var template = <p> Hello Babel from Webpack</p>
-
-ReactDOM.render(template, document.getElementById('app'));
